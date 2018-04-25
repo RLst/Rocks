a8eeaa870a0e6fad8951e48b9a6b4cab254f86fe
Tony Le; BINDOZE; MacBook Bootcamp <letony@icloud.com>
2018-04-25 21:51:24 +1000

    Renamed class RockApp to Game

---
+++ Rocks/Game.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "Application.h"
+#include "Renderer2D.h"
+
+#include "SpaceFighter.h"
+#include "Bullet.h"
+
+class SpaceFighter;
+
+class Game : public aie::Application {
+public:
+
+	Game();
+	virtual ~Game();
+
+	virtual bool startup();
+	virtual void shutdown();
+
+	virtual void update(float deltaTime);
+	virtual void draw();
+
+protected:
+
+	aie::Renderer2D*	m_2dRenderer;
+	aie::Font*			m_font;
+
+	//Player*				m_player;
+	//float				m_timer;
+
+	SpaceFighter		m_player;
+	Bullet*				m_bullets;
+	
+};
