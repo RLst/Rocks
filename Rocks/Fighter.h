5f1166ba7a0f0cce18bf301124a55642a2f06ab8
Minh Huy Le <s181335@students.aie.edu.au>
2018-04-26 11:06:07 +1000

    Renamed SpaceFighter to Fighter

---
+++ Rocks/Fighter.h
@@ -0,0 +1,29 @@
+#pragma once
+
+//References and prototype
+#include <glm\vec2.hpp>
+namespace aie
+{
+	class Texture;
+	class Renderer2D;
+	class Input;
+}
+class Bullet;
+
+
+class SpaceFighter
+{
+	aie::Texture*	m_player;
+	glm::vec2		m_playerPos;
+	float			m_playerAngle;
+	float			m_playerSpeed;
+	//float			m_playerPosX;
+	//float			m_playerPosY;
+
+public:
+	SpaceFighter();
+	~SpaceFighter();
+	void update(float deltaTime, aie::Input* input);
+	void draw(aie::Renderer2D* spriteBatch);
+};
+
