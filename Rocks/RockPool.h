30969ba524d3694b9e52817d544917db8ccc5e41
Tony Le; BINDOZE; MacBook Bootcamp <letony@icloud.com>
2018-04-28 21:56:38 +1000

    Actual textures for the rocks will be stored inside RockPool not Rock; Rock will point to the textures

--- Rocks/RockPool.h
+++ Rocks/RockPool.h
@@ -18,6 +18,11 @@ private:
 	Rock*			m_rocks;			//Rock array
 	Rock*			m_firstAvailable;
 
+	//Pointers to the rock textures
+	aie::Texture*	m_rock_sml;
+	aie::Texture*	m_rock_med;
+	aie::Texture*	m_rock_lge;
+
 public:
 	RockPool();
 	RockPool(unsigned int);
