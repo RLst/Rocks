7475c69eb020ed11be9b7245541001189209f590
Tony Le; BINDOZE; MacBook Bootcamp <letony@icloud.com>
2018-04-28 21:50:44 +1000

    Some basic enums and global Random()

--- Rocks/GameDefines.h
+++ Rocks/GameDefines.h
@@ -1,9 +1,19 @@
 #pragma once
 
+//Game settings
 const unsigned int const SCREEN_WIDTH = 1440;
 const unsigned int const SCREEN_HEIGHT = 900;
 
-int Random(int min, int max);namespace pkr {	//pokoro
+//Enums
+enum RockType {
+	SML_ROCK = 1,
+	MED_ROCK,
+	LGE_ROCK
+};
+
+//Core functions
+int Random(int min, int max);
+
 namespace pkr {	//pokoro
 //Flexi vector
 
