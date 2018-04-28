8808ab14db9956dd9c6db90475f0f0d2161a3b64
Tony Le; BINDOZE; MacBook Bootcamp <letony@icloud.com>
2018-04-28 21:55:23 +1000

    Basic free list object pool for rocks

--- Rocks/RockPool.h
+++ Rocks/RockPool.h
@@ -1,12 +1,26 @@
 #pragma once
+
+//#include <Texture.h>		//Won't work without this!!!
+//#include "Rock.h"
+namespace aie {
+	class Renderer2D;
+	class Texture;
+}
+
 namespace pkr {
 	class Rock;
 	class Fighter;
 
 class RockPool
 {
+private:
+	unsigned int	MAX_ROCKS = 100;	//Default 100
+	Rock*			m_rocks;			//Rock array
+	Rock*			m_firstAvailable;
+
 public:
 	RockPool();
+	RockPool(unsigned int);
 	~RockPool();
 };
 
