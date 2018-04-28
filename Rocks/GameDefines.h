a35b1e1bdc21bd037a67f647a65cb1c799cd0897
Tony Le; BINDOZE; MacBook Bootcamp <letony@icloud.com>
2018-04-28 21:51:35 +1000

    Tried implementing my own vector2 class, but ran into roadblocks. Will keep using glm::vec2 instead

--- Rocks/GameDefines.h
+++ Rocks/GameDefines.h
@@ -17,5 +17,57 @@ int Random(int min, int max);
 namespace pkr {	//pokoro
 //Flexi vector
 
+//typename<typename T>
+union vector2
+{
+	//Position and trajectories
+	struct {
+		float x;
+		float y;
+	};
+
+	//Velocity (Speed and angle)
+	struct {
+		float m;	//magnitude
+		float a;	//angle
+	};
+
+	//Operator overloads (only have to do for x,y since the share mem with m,a)
+	//Getting HAIRY!!!..
+	vector2& operator + (const vector2 &other) {
+		vector2 add;
+		add.x += other.x;
+		add.y += other.y;
+		return add;
+	}
+	vector2& operator - (const vector2 &other) {
+		vector2 sub;
+		sub.x -= other.x;
+		sub.y -= other.y;
+		return sub;
+	}
+	vector2& operator * (const vector2 &other) {
+		vector2 mult;
+		mult.x *= other.x;
+		mult.y *= other.y;
+		return mult;
+	}
+	vector2& operator / (const vector2 &other) {
+		vector2 div;
+		div.x *= other.x;
+		div.y *= other.y;
+		return div;
+	}
+	bool operator == (vector2 const &other) {
+		return (x == other.x && y == other.y);
+	}
+	vector2& operator = (vector2 const &rhs) {
+		this->x = rhs.x;
+		this->y = rhs.y;
+		return *this;
+	}
+
+};
+
 
 }
